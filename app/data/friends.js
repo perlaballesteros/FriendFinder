068551ddfd9b1e5fae878a6cd1d7aac62e4d4158
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-02-05 14:59:14 -0700

    finished the public html

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends=[
+    {
+        name:"Jane Doe",
+        photo:"http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Female-Face-FB-5-icon.png",
+        scores:[1,2,3,4,5,5,4,3,2,1]
+    }
+];
+
+module.exports=friends;
