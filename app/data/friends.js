30a8d5a906cd8e829335f83f1774aa20f955f549
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-02-05 23:07:27 -0700

    working on displaying the match

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,11 @@ var friends=[
         name:"Jane Doe",
         photo:"http://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Female-Face-FB-5-icon.png",
         scores:[1,2,3,4,5,5,4,3,2,1]
+    },
+    {
+        name:"Julio Cesar",
+        photo:"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE5NTU2MzE1ODU5NTUxNzU1/julio-cesar-chavez-37217-1-raw.jpg",
+        scores:[5,2,5,2,1,1,4,3,2,1]
     }
 ];
 
