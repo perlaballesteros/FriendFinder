d79e42d51db9fda4a8d46a045cfce64870dcaa45
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-02-08 10:38:06 -0700

    basic friend finder works

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,26 +1,11 @@
 var friends = require("../data/friends");
 
 module.exports = function(app) {
-   
-    app.get("/api/friends", function(req, res) {
+      app.get("/api/friends", function(req, res) {
       res.json(friends);
     });
-
     app.post("/api/friends", function(req, res) {
        friends.push(req.body);
-      
-   });
-  
-    /*// ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-  
-    app.post("/api/clear", function() {
-      // Empty out the arrays of data
-      tableData = [];
-      waitListData = [];
-  
-      console.log(tableData);
-    });*/
-  };
+    });
+};
   
